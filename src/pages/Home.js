7b8188b9b85761957ed593372f91211648e5006c
TrungCorp <trungle9995@gmail.com>
2024-06-05 12:57:08 -0400

    outputs some data on home and technique page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,31 @@ import NavBar from "../components/NavBar";
 import {useEffect,useState} from "react";
 
 function Home(){
-  
+    const [users,setUsers] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:3000/techniqueUsers")
+        .then(resp=> resp.json())
+        .then(data =>setUsers(data))
+  },[])
+
+  const userList = users.map((user)=> {
+    return <article key={user.name}>
+        <h2>{user.name}</h2>
+        <h2>Techiques:
+            <ul>
+                {user.techniques.map(technique => <li>{technique}</li>)}
+            </ul>
+        </h2>
+    </article>
+  })
     return(
         <div>
             <header>
                 <NavBar/>
             </header>
             HOME PAGE
+
+            {userList}
         </div>
     )
 }
