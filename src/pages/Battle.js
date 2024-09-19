17bc9aa9ce0f60561ad1976a9d387340c5b8700f
TrungCorp <trungle9995@gmail.com>
2024-09-19 13:50:50 -0400

    created Battle.css

--- src/pages/Battle.js
+++ src/pages/Battle.js
@@ -1,4 +1,12 @@
+import { useOutletContext } from "react-router-dom"
+
 function Battle(){
+    const {players} = useOutletContext()
+    console.log(players)
+
+    function handleBattle(){
+        console.log("BATTLE.JS TEST")
+    }
     return(
         <div>
             BATTLE PAGE
